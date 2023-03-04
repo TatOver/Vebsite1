5cba8fb4f506a36f1c06b89a8c9701e66f42666c
Пользователь <trollka@list.ru>
2023-03-04 15:54:59 +0300

    add page about

--- vebsite1/main/urls.py
+++ vebsite1/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index ),
-    path('contacts/',views.contact ),
+    path('contacts/',views.contact),
+    path('about-us/',views.about),
 ]
--- vebsite1/main/views.py
+++ vebsite1/main/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h4>Hello</h4>')
-
+    # return render(request,'')
+    return HttpResponse('<h2>Hello</h2>')
 def contact(request):
     return HttpResponse('<h2>Контакты</h2>')
+
+def about(request):
+    return HttpResponse('<h2>About us</h2>')
+
