68377515d1090be2040c93cf7bb81f6645ccd575
Пользователь <trollka@list.ru>
2023-03-18 15:27:39 +0300

    add migrations

--- vebsite1/main/views.py
+++ vebsite1/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def index(request):
-    return render(request,'main/index.html')
+    tasks=Task.objects.all()
+    return render(request,'main/index.html',{'title':'Главная страница', 'tasks':tasks})
     # return HttpResponse('<h2>Hello</h2>')
 def contact(request):
     return render(request,'main/contact.html')
