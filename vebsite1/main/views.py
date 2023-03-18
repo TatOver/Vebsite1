5257a985ee7ee23825563f8a18eef70f336b2966
Пользователь <trollka@list.ru>
2023-03-18 21:51:08 +0300

    page form

--- vebsite1/main/views.py
+++ vebsite1/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
 
+from .forms import TaskForm
+
 def index(request):
     tasks=Task.objects.all()
     return render(request,'main/index.html',{'title':'Главная страница', 'tasks':tasks})
@@ -13,5 +15,9 @@ def about(request):
     return render(request,'main/about.html')
 
 def create(request):
+    form = TaskForm()
+    context={
+        'form': form
+    }
     return render(request,'main/create.html')
 
