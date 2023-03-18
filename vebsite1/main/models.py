881088c94a0599942023c6429bd4ccf01a9cd1e9
Пользователь <trollka@list.ru>
2023-03-18 13:34:46 +0300

    add model

--- vebsite1/main/models.py
+++ vebsite1/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+    surname = models.CharField('Исполнитель', max_length=60)
+
+    def __str__(self):
+        return self.title
