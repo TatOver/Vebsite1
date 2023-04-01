309f0dd6c446074d4bc827187a653ce2655b26c8
Пользователь <trollka@list.ru>
2023-04-01 11:30:55 +0300

    add fields in  form

--- vebsite1/main/forms.py
+++ vebsite1/main/forms.py
@@ -1,8 +1,12 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title","task","surname"]
+        widgets = {"title": TextInput(attrs={'class': 'form-control','placeholder': 'Введите название'}),
+                   "task": TextInput(attrs={'class': 'form-control','placeholder': 'Введите описание'}),
+                   "surname": TextInput(attrs={'class': 'form-control','placeholder': 'Введите фамилию исполнителя'}),
+                   }
 
